5427cd6209d15988e9090821414f391f80ccceb4
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-24 17:14:23 +0100

    installed bootstrap

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,7 @@
 
 <template>
     <div>
-        asd
+  
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import './styles/styles.scss'
 
 const app = createApp(App)
