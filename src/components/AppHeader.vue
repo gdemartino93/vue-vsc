9751b67bc2dad29234fdd9b2adabb76a19c3ac19
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-24 18:34:41 +0100

    dropdown menu at mobile version

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,14 +12,26 @@ import vscIcon from "@/assets/icons/vsc-icon.svg";
 </script>
 
 <template>
+
     <div class="col-12 cmd-bar">
         <div class="col-4 left-side">
             <ul class="d-flex gap-2 align-items-center">
                 <li><img :src="vscIcon" alt="Icona" ></li>
-                <li>File</li>
-                <li class="d-none d-md-block" v-for="menuOption in menuOptions">
-                    <span>{{ menuOption }}</span>
-                </li>
+
+                <!-- menu non dropdown mostrato al brekpoint xl -->
+                <div class="gap-2 align-items-center d-none d-xl-block d-xl-flex">  
+                    <li>File</li>
+                    <li v-for="menuOption in menuOptions">
+                        <span>{{ menuOption }}</span>
+                    </li>
+                </div>
+                <!-- menu dropdown nascosto al breakpoint xl  -->
+                <div class="d-xl-none" >
+                    <li class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">File</li>
+                    <ul class="dropdown-menu myDropDown">
+                        <li v-for="(menuOption,index) in menuOptions" :key="index"><a class="dropdown-item" href="#">{{ menuOption }}</a></li>
+                    </ul>
+                </div>
             </ul>
         </div>
         
@@ -40,14 +52,22 @@ import vscIcon from "@/assets/icons/vsc-icon.svg";
         margin-left: -20px;
     }
     ul{
+        // background-color: var(--color-bg-code);
+        color: var(--color-text);
         li{
             cursor: pointer;
             padding: 5px;
+            a{
+                color: var(--color-text);
+            }
             &:hover{
                 background-color: var(--hover-bg-bar);
                 border-radius: 5px;
             }
         }
+    }
+    .myDropDown{
+        background: var(--color-bg-side);
     }
 }
 </style>
