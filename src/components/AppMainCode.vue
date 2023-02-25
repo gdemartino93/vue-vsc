292cf324c9b415fc75719e6432acfa340e80b00a
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-26 00:06:20 +0100

    working on form

--- src/components/AppMainCode.vue
+++ src/components/AppMainCode.vue
@@ -7,14 +7,14 @@
       <div class="image-home col-12  col-lg-6 d-flex mx-auto animate__animated animate__fadeIn my-4 my-lg-0" v-if="store.scritturaTerminata">
         <img :src="codeImg" alt="" class="img-fluid">
       </div>
-      <form v-if="store.scritturaTerminata" action="" netlify class="flex-column col-12 col-md-8 col-lg-6 animate__animated animate__fadeIn my-4 my-lg-0">
+      <form v-if="store.scritturaTerminata" action="" method="POST" name="contact" class="flex-column col-12 col-md-8 col-lg-6 animate__animated animate__fadeIn my-4 my-lg-0" netlify>
         <div class="mb-3">
           <label for="exampleFormControlInput1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
         </div>
         <div class="mb-3">
           <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          <input class="form-control" id="exampleFormControlTextarea1" rows="3" type="text" name="name" />
         </div>
         <button type="submit" class="btn btn-success">Send</button>
       </form>
