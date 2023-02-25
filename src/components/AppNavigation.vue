85395fe45d91521fe26e4a28af5df09f1fa35f18
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-25 12:02:15 +0100

    create navigation component

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template>
+    <div class="navigation-bar">
+
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.navigation-bar{
+    width: calc(100% - 280px);
+    height: 50px;
+    background:var(--color-bg-aside-expanded);
+    flex: 1;
+    position: absolute;
+    right: 0;
+}
+</style>
