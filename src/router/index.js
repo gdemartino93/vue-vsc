e875b815f4aabf43135d800ba3e968bb48ecda20
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-26 19:11:26 +0100

    create contacts route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
 import CV from '../views/pages/CV.vue';
 import About from '../views/pages/About.vue';
+import Contacts from '../views/pages/Contacts.vue';
 import Home from '../components/AppMainCode.vue'
 
 const router = createRouter({
@@ -13,20 +14,20 @@ const router = createRouter({
       component: Home
     },
     {
-      path: '/cv',
-      name: 'cv',
-      component: CV
-    },
-    {
       path: '/about',
       name: 'about',
       component: About
     },
     {
+      path: '/contacts',
+      name: 'contacts',
+      component: Contacts
+    },
+    {
       path: '/cv',
       name: 'cv',
       component: CV
-    }
+    },
   ]
 })
 
