36364c3fd9cbf6c1fa6b372dbe76c39186f2d405
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-26 17:02:51 +0100

    form fix

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,9 +1,13 @@
    <template>
     <section class="col-10 col-md-8 col-lg-10 mx-auto d-flex flex-column gap-5 animate__animated animate__fadeIn" @submit.prevent="sendMail" v-if="store.scritturaTerminata">
       <div class="form-container">
-        <h3 class="text-center fw-bold mb-4">Contattami:</h3>
-        <p class="mb-4">Se hai domande, suggerimenti o sei interessato ad una collaborazione, non esitare a contattarmi compilando il seguente form.</p>
-        <form class="col-12 d-flex flex-column gap-3" ref="myForm">
+        <h3 class="fw-bold fs-3 mb-4 tag">.Contattami {</h3>
+
+        <p class="testo">Se hai domande o suggerimenti non esitare a contattarmi compilando il seguente form.</p>
+
+        <span class="fw-bold fs-3">}</span>
+
+        <form class="col-12 d-flex flex-column gap-3 mt-4" ref="myForm">
           <div class="form-group">
             <input name="from_name" placeholder="Inserisci il tuo nome" type="text" v-model="from_name" class="form-control input-contact" v-bind:required="true">
           </div>
@@ -56,6 +60,13 @@
   }
   </script>
   <style lang="scss" scoped>
+  *{
+  letter-spacing: 1px;
+  }
+  .testo{
+    line-height: 40px;
+    padding-left: 50px;
+  }
   .input-contact{
     background: var(--color-bg-aside-expanded);
     &:focus{
