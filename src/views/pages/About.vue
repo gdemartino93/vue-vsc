f9c83bf0f5fbe1157ebcdacc36d75586a7efbf20
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-01 02:04:51 +0100

    about section p

--- src/views/pages/About.vue
+++ src/views/pages/About.vue
@@ -15,41 +15,41 @@ export default {
 <template>
   <section class="main-code padding-lg">
     <div class="col-10 col-md-8 col-lg-10 d-flex flex-column justify-content-center mx-auto">
-      <!-- Qui viene mostrato un paragrafo con informazioni personali -->
-      <p class="">...</p>
-    </div>
-    
-    <!-- Qui viene mostrata la sezione Front-end -->
-    <div class="col-10 col-md-8 col-lg-10 d-flex flex-column mx-auto">
-      <h3 class="text-center mb-4 fw-bold fs-2 mt-3">Tecnologie Front-End</h3>
-      
-      <!-- Vengono mostrate le immagini delle tecnologie di front-end presenti nello store -->
-      <div v-for="skill in store.skills" class="d-flex justify-content-between flex-wrap gap-5">
-        <div v-for="skillFront in skill.frontEnd" class="box col-3 justify-content-center align-items-center">
-          <img :src="`/assets/${skillFront.img}`" :alt="skillFront.name">
-        </div>
+        <h3 class="text-center fw-bold fs-3 mb-4">Parlo di me:</h3>
+        <!-- parte testo -->
+        <p class="">Mi chiamo Gianluca De Martino ed ho 29 anni. <br>
+          La passione per l'informatica è nata quando ero ancora bambino. Infatti, nonostante la mia giovane età, ho un ricordo nitido del modem 56k e del relativo suono, ma anche dell'icona di Internet Explorer che doveva completare il giro del mondo per caricare la pagina.  <br>
+          Mi piace programmare e rimanere sempre aggiornato su nuove tecnologie e qualcosa l'ho finalmente imparata, credo...🤷‍♂️ <br>
+          Ogni volta che posso, mi diverto a "fare cose" con le librerie e i framework più moderni di JavaScript, come React.js e Vue.js, ma non mi dispiace anche sperimentare con Laravel. <br>
+          Inoltre <span class="fw-bold"> sono fiero di poter dire che ho creato questo sito portfolio da zero </span>  ed oltre ad essere un modo per mettere in pratica le mie abilità spero che possa anche dimostrare le mie competenze e soprattutto <span class="fw-bold">la mia passione per questo mondo.</span>  <br>
+          Il mio obiettivo è continuare a crescere come sviluppatore e contribuire alla realizzazione ed al mantenimento di progetti innovativi.
+        </p>
       </div>
-      
-      <!-- Qui viene mostrata la sezione Back-end -->
-      <h3 class="text-center my-4 fw-bold fs-2">Tecnologie Back-End</h3>
-      
-      <!-- Vengono mostrate le immagini delle tecnologie di back-end presenti nello store -->
-      <div v-for="skill in store.skills" class="d-flex justify-content-between flex-wrap gap-5">
-        <div v-for="skillBack in skill.backEnd"  class="box col-3 justify-content-center align-items-center">
-          <img :src="`/assets/${skillBack.img}`" :alt="skillBack.name">
-        </div>
+      <!-- fine parte testo -->
+      <!-- sezione frontend -->
+      <div class="col-10 col-md-8 col-lg-10 d-flex flex-column mx-auto">
+        <h3 class="text-center mb-4 fw-bold fs-2 mt-3">Tecnologie Front-End</h3>
+          <div v-for="skill in store.skills" class="d-flex justify-content-between flex-wrap gap-5">
+
+            <div v-for="skillFront in skill.frontEnd" class="box col-3 justify-content-center align-items-center">
+                <img :src="`/assets/${skillFront.img}`" :alt="skillFront.name">
+            </div>
+          </div>
+          <!-- sezione backend -->
+          <h3 class="text-center my-4 fw-bold fs-2">Tecnologie Back-End</h3>
+          <div v-for="skill in store.skills" class="d-flex justify-content-between flex-wrap gap-5">
+            <div v-for="skillBack in skill.backEnd"  class="box col-3 justify-content-center align-items-center">
+                <img :src="`/assets/${skillBack.img}`" :alt="skillBack.name">
+            </div>
+          </div>
+          <!-- sezione strumenti utilizzati -->
+          <h3 class="text-center my-4 fw-bold fs-2">Strumenti che utilizzo</h3>
+          <div v-for="skill in store.skills" class="d-flex justify-content-between flex-wrap gap-5 mb-5">
+            <div v-for="strumenti in skill.strumentiUtilizzati"  class="box col-3 justify-content-center align-items-center">
+                <img :src="`/assets/${strumenti.img}`" :alt="strumenti.name">
+            </div>
+          </div>
       </div>
-      
-      <!-- Qui viene mostrata la sezione Strumenti utilizzati -->
-      <h3 class="text-center my-4 fw-bold fs-2">Strumenti che utilizzo</h3>
-      
-      <!-- Vengono mostrate le immagini degli strumenti utilizzati presenti nello store -->
-      <div v-for="skill in store.skills" class="d-flex justify-content-between flex-wrap gap-5 mb-5">
-        <div v-for="strumenti in skill.strumentiUtilizzati"  class="box col-3 justify-content-center align-items-center">
-          <img :src="`/assets/${strumenti.img}`" :alt="strumenti.name">
-        </div>
-      </div>
-    </div>
   </section>
 </template>
 
