0ffb3c9a213ef60c1c22f98ae6de9826e1bfe80a
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-18 13:19:48 +0100

    created component

---
+++ src/components/ChatAI.vue
@@ -0,0 +1,100 @@
+<template>
+  <section class="main-code padding-lg">
+    <div class="col-10 col-md-8 col-lg-10 d-flex flex-column justify-content-center mx-auto">
+      <h3 class="text-center fw-bold fs-3 mb-4">Parlo di me:</h3>
+      <div class="col-4">
+        <form action="" method="post" @submit.prevent="getReply">
+          <input class="form-control" type="text" placeholder="Default input" aria-label="default input example" v-model="message">
+          <button class="btn btn-success" type="submit">Invia</button>
+        </form>
+      </div>
+      <div v-for="frase in frasi">{{ frasi }}</div>
+      <div v-html="risposta"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      message: '',
+      frasi:[],
+      risposta: '',
+      OPENAI_API_KEY: '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',
+    };
+  },
+  methods: {
+    getReply() {
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.OPENAI_API_KEY
+      };
+      const data = {
+        'model': 'gpt-3.5-turbo',
+        'messages': [{'role': 'user', 'content': this.message}],
+        'temperature': 0.7
+      };
+      axios.post('https://api.openai.com/v1/chat/completions', data, { headers })
+        .then(response => {
+          this.frasi = [...this.frasi, this.message];
+          this.risposta += response.data.choices[0].message.content + "<br>";
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+}
+</script>
+
+
+  <style lang="scss" scoped>
+  *{
+    letter-spacing: 1px;
+  }
+  
+  
+  .main-code {
+    background-color: var(--color-bg-code);
+    color: var(--color-text);
+    flex: 1;
+    min-height: 100vh;
+  
+  }
+  
+  .padding-lg {
+    padding-top: 60px !important;
+    padding-left: 60px !important;
+  }
+  
+  @media (min-width: 990px) {
+    .padding-lg {
+      padding-top: 100px !important;
+      padding-left: 247px !important;
+    }
+  }
+  
+  @media (min-width: 400px) and (max-width : 600px) {
+    .padding-xs {
+      padding-top: 60px !important;
+      padding-left: 80px !important;
+    }
+  }
+  .box{
+    display: flex;
+    justify-content: centers;
+    border: 1px solid cornflowerblue;
+    height: 100px;
+    &:hover{
+      box-shadow: 5px 5px cadetblue;
+      scale: 1.2;
+    }
+    img{
+      width: 3rem;
+    }
+  }
+  
+  </style>
